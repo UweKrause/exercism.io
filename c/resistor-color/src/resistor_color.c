d3194c2e3c0657483ca1b66ac0a8169a187d7777
Uwe Krause <16082815+UweKrause@users.noreply.github.com>
2021-02-10 16:05:31 +0100

    step 1: get rid of compiler errors

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -1,1 +1,17 @@
 #include "resistor_color.h"
+
+int color_code(resistor_band_t band) {
+    
+    // dummy implementation, just to get rid of compiler errors
+    return (int) band;
+}
+
+resistor_band_t colors() {
+    
+    // just copied from the testcases for now, just to get rid of compiler errors
+    resistor_band_t expected[] = {
+        BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE        
+    };
+   
+   return *expected;
+}
--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -1,8 +1,13 @@
 #ifndef RESISTOR_COLOR_H
 #define RESISTOR_COLOR_H
 
+// just copied from the testcases for now, just to get rid of compiler errors
 typedef enum {
-
+    BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE
 } resistor_band_t;
+
+int color_code(resistor_band_t band);
+
+resistor_band_t colors();
 
 #endif
