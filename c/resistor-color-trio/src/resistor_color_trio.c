b1fa1f2d547328def622982a76f6c1a5eab0d73b
Uwe Krause <16082815+UweKrause@users.noreply.github.com>
2021-02-10 19:21:28 +0100

    just enought for the compiler

---
+++ c/resistor-color-trio/src/resistor_color_trio.c
@@ -0,0 +1,12 @@
+#include "resistor_color_trio.h"
+
+resistor_value_t color_code(resistor_band_t band[])
+{
+    // ignore unused
+    (void)band;
+    
+    // dummy data
+    resistor_value_t value = { 42, KILOOHMS};
+
+    return value;
+}
