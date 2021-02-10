c2c8eb03927e8f9b869e0e128655e07f081ce441
Uwe Krause <16082815+UweKrause@users.noreply.github.com>
2021-02-10 19:02:55 +0100

    resistor color duo

--- c/resistor-color-duo/main/main.c
+++ c/resistor-color-duo/main/main.c
@@ -3,7 +3,12 @@
 
 int main() {
     
-    int cc = color_code((resistor_band_t[]){ BROWN, BLACK });
+    // when called with less than 2 colors, unexpected things will happen
+    printf("color code: %d\n", color_code((resistor_band_t[]){ BROWN }));
+    
+    // works as intended
+    printf("color code: %d\n", color_code((resistor_band_t[]){ BROWN, ORANGE, RED , YELLOW}));
         
-    printf("color code: %d\n", cc);
+    
+        
 }
--- c/resistor-color-duo/src/resistor_color_duo.c
+++ c/resistor-color-duo/src/resistor_color_duo.c
@@ -1,9 +1,8 @@
 #include "resistor_color_duo.h"
 
-int color_code(resistor_band_t* band) {
-    
-    // silence warning unused variable for the moment
-    (void)band;
-    
-    return 15;
+int color_code(resistor_band_t band[])
+{
+    // even if it is working, this feels wrong (probably because it is...)
+    // but I cant find a way to check for existence of the second value
+    return band[0] * 10 + band[1];
 }
