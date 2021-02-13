f407a5c7b3bf62acba407a6bc7fff5af856345ec
Uwe Krause <16082815+UweKrause@users.noreply.github.com>
2021-02-13 01:45:48 +0100

    fixed a brainfart

--- c/resistor-color-trio/src/resistor_color_trio.c
+++ c/resistor-color-trio/src/resistor_color_trio.c
@@ -1,12 +1,34 @@
 #include "resistor_color_trio.h"
+#include <math.h>
+
+int color_code_duo(resistor_band_t band[])
+{
+    return band[0] * 10 + band[1];
+}
 
 resistor_value_t color_code(resistor_band_t band[])
 {
-    // ignore unused
-    (void)band;
+    int value = 0;
+    unit_t unit = OHMS;
     
-    // dummy data
-    resistor_value_t value = { 42, KILOOHMS};
-
-    return value;
+    int exponent = band[2];
+    
+    /*
+     * wrap over when value gets longer than 3 digits (aka. thousand or more)
+     *
+     * The first 2 bands already give two digits (10)
+     * one digit more is okay (exponent equals 1) (100)
+     * the fourth digit would be the first thousands (exponent > 1) (1000),
+     * and therefore must be wrapped.
+     */
+    if (exponent > 1) {
+        unit = KILOOHMS;
+        exponent -= 3;
+    }
+    
+    value = color_code_duo(band) * pow(10, exponent);
+    
+    resistor_value_t capacity = {value, unit};
+    
+    return capacity;
 }
--- c/resistor-color-trio/src/resistor_color_trio.h
+++ c/resistor-color-trio/src/resistor_color_trio.h
@@ -1,27 +1,31 @@
 #ifndef RESISTOR_COLOR_TRIO_H
 #define RESISTOR_COLOR_TRIO_H
 
-    typedef enum {
-        BLACK   = 0,
-        BROWN   = 1,
-        RED     = 2,
-        ORANGE  = 3,
-        YELLOW  = 4,
-        GREEN   = 5,
-        BLUE    = 6,
-        VIOLET  = 7,
-        GREY    = 8,
-        WHITE   = 9
-    } resistor_band_t;
-    
-    #define OHMS "ohms"
-    #define KILOOHMS "kiloohms"
-        
-    typedef struct resistor_value_t {
-        int value;
-        char unit[9]; // make enough space for the string (plus \0 )
-    } resistor_value_t;
-    
-    resistor_value_t color_code(resistor_band_t band[]);
+typedef enum {
+    BLACK   = 0,
+    BROWN   = 1,
+    RED     = 2,
+    ORANGE  = 3,
+    YELLOW  = 4,
+    GREEN   = 5,
+    BLUE    = 6,
+    VIOLET  = 7,
+    GREY    = 8,
+    WHITE   = 9
+} resistor_band_t;
+
+typedef enum {
+    OHMS,
+    KILOOHMS
+} unit_t;
+
+typedef struct resistor_value_t {
+    int value;
+    unit_t unit;
+} resistor_value_t;
+
+int color_code_duo(resistor_band_t band[]);
+
+resistor_value_t color_code(resistor_band_t band[]);
 
 #endif
