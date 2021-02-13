a5e1d867c78cb453adf5b9b9d3d0d435dcda1403
Uwe Krause <16082815+UweKrause@users.noreply.github.com>
2021-02-13 15:20:33 +0100

    add explanation to usage commentary

--- c/resistor-color-duo/main/main.c
+++ c/resistor-color-duo/main/main.c
@@ -3,12 +3,15 @@
 
 int main() {
     
-    // when called with less than 2 colors, unexpected things will happen
+    /*
+     * when called with less than 2 arguments, the second item will be initialized with 0
+     * (and semantic analysis in IDE complains)
+     */
     printf("color code: %d\n", color_code((resistor_band_t[]){ BROWN }));
     
     // works as intended
     printf("color code: %d\n", color_code((resistor_band_t[]){ BROWN, ORANGE, RED , YELLOW}));
-        
     
-        
+    
+    
 }
