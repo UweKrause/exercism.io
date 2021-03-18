58bf1378119620550ce390d58d3c85088ced2af5
Uwe Krause <16082815+UweKrause@users.noreply.github.com>
2021-03-18 21:17:14 +0100

    Isogram

--- c/isogram/src/isogram.c
+++ c/isogram/src/isogram.c
@@ -1,6 +1,38 @@
 #include "isogram.h"
 
+int position_of_letter_in_alphabet(char c);
+
 bool is_isogram(const char phrase[]) {
-    phrase = phrase; // just to silence the compiler for now
+
+    if (!phrase) return false;
+
+    bool alphabet[26] = {false};
+
+    for (int i = 0; phrase[i]; ++i) {
+        int position = position_of_letter_in_alphabet(phrase[i]);
+        if (position >= 0) {
+
+            // if this letter occurred before, this phrase is not an isogram.
+            if (alphabet[position]) return false;
+
+            // otherwise remember occurrence of this letter
+            alphabet[position] = true;
+        }
+    }
     return true;
+}
+
+/**
+ * When a letter is in the alphabet (ascii), return its position.
+ *
+ * first is 0, last is 25
+ * not in alphabet is -1
+ *
+ * @param c the letter
+ * @return the position in the alphabet; -1 when argument is no ascii letter
+ */
+int position_of_letter_in_alphabet(char c) {
+    if (c >= 'a' && c <= 'z') return c - 'a';
+    if (c >= 'A' && c <= 'Z') return c - 'A';
+    return -1;
 }
--- c/isogram/src/isogram.h
+++ c/isogram/src/isogram.h
@@ -3,6 +3,15 @@
 
 #include <stdbool.h>
 
+/**
+  * An isogram (also known as a "nonpattern word") is a word or phrase without a repeating letter.
+  *
+  * Spaces and hyphens are allowed to appear multiple times.
+  * Currently only works for ascii: umlauts or accents are silently ignored.
+  *
+  * @param phrase Phrase to check if it is an isogram
+  * @return true if isogram; false otherwise
+  */
 bool is_isogram(const char phrase[]);
 
 #endif
