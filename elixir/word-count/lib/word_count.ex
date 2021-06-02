1fb985a8683b60e88a0efaf2277b8067e6aa1ba2
Uwe Krause <16082815+UweKrause@users.noreply.github.com>
2021-06-02 09:50:31 +0200

    working sulution. Not optimal yet

--- elixir/word-count/lib/word_count.ex
+++ elixir/word-count/lib/word_count.ex
@@ -6,5 +6,20 @@ defmodule WordCount do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    words = Regex.scan(~r/[1-9a-zA-ZßäöüÄÖÜ-]{1,}/u, sentence)
+    count(words, %{})
   end
+
+  def count([[word] | words_remaining], result) do
+    key = String.downcase(word)
+    value = Map.get(result, key, 0)
+    {_, result} = Map.get_and_update(result, key, fn current -> {current, value + 1} end)
+
+    count(words_remaining, result)
+  end
+
+  def count([], result) do
+    result
+  end
+
 end
