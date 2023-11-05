3a69d15b45f75f88a2e5157313b78d5b68dc5f77
Uwe Krause <uwe@uwekrau.se>
2023-11-05 23:37:18 +0100

    changed to a (in my opinion) suboptimal version, because of automatic feedback: "The purpose of this exercise is to teach `cond` Use `cond` in both to_label and alert_recipient."

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,28 +1,27 @@
-defmodule LogLevel do
-  def to_label(0, false), do: :trace
-  def to_label(1, _), do: :debug
-  def to_label(2, _), do: :info
-  def to_label(3, _), do: :warning
-  def to_label(4, _), do: :error
-  def to_label(5, false), do: :fatal
-  def to_label(_level, _legacy?), do: :unknown
+# changed to a (in my opinion) suboptimal version, because of automatic feedback:
+# "The purpose of this exercise is to teach `cond` Use `cond` in both to_label and alert_recipient."
 
-  def alert_recipient(level, legacy?) do
-    to_label(level, legacy?)
-    |> get_recipient(legacy?)
+defmodule LogLevel do
+  def to_label(level, legacy?) do
+    cond do
+      level == 0 and not legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 and not legacy? -> :fatal
+      true -> :unknown
+    end
   end
 
-  # If the log label is error or fatal, send the alert to the ops team.
-  defp get_recipient(:error, _), do: :ops
-  defp get_recipient(:fatal, _), do: :ops
+  def alert_recipient(level, legacy?) do
+    label = to_label(level, legacy?)
 
-  # If you receive a log with an unknown label from a legacy system,
-  # send the alert to the dev1 team,
-  defp get_recipient(:unknown, true), do: :dev1
-
-  # other unknown labels should be sent to the dev2 team.
-  defp get_recipient(:unknown, _), do: :dev2
-
-  # All other log labels can be safely ignored by returning false.
-  defp get_recipient(_, _), do: false
+    cond do
+      label == :error or label == :fatal -> :ops
+      label == :unknown and legacy? -> :dev1
+      label == :unknown -> :dev2
+      true -> false
+    end
+  end
 end
